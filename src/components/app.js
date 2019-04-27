bcb03ff719797937021a139db87b088a1cacfdf2
Rebecca Martin <rebecca-martin@live.com>
2019-04-27 12:54:48 -0700

    update

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,16 @@
+import React from 'react';
+import Table from './table';
 
+const students = [
+    { student: 'reed', course: 'history', grade: 87 }, { student: 'leyla', course: 'science', grade: 93 }, { student: 'doug', course: 'math', grade: 64 }
+];
 
+export default () => {
+    return <div className="container">
+
+        <div><h1>Student Grade Table</h1></div>
+
+        <Table data={students} />
+
+    </div>
+}
